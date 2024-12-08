ecba39c836e10bc0accf3b0770955217b065c1ce
Martin Schijf <martinschijf@bol.com>
2024-12-08 09:43:15 +0100

    December 8 refactoring

--- src/main/kotlin/adventofcode/Day08.kt
+++ src/main/kotlin/adventofcode/Day08.kt
@@ -16,29 +16,33 @@ class Day08(test: Boolean) : PuzzleSolverAbstract(test, puzzleName="Resonant Col
         .mapValues { groupedEntry -> groupedEntry.value.map { it.key } }
 
     override fun resultPartOne(): Any {
-        val allAntinodeLocations = antennaGroupMap.values.flatMap { it.determineAntinodeLocations() }.toSet()
+        val allAntinodeLocations = antennaGroupMap.values.flatMap { it.determineAntinodeLocationsOneStep() }.toSet()
         return allAntinodeLocations.size
     }
 
     override fun resultPartTwo(): Any {
-        val allAntinodeLocations = antennaGroupMap.values.flatMap { it.determineAntinodeLocations2() }.toSet()
+        val allAntinodeLocations = antennaGroupMap.values.flatMap { it.determineAntinodeLocationsInLine() }.toSet()
         return allAntinodeLocations.size
     }
 
-    private fun List<Point>.determineAntinodeLocations(): Set<Point> {
-        val x =  this.toCombinedItemsList().flatMap{oneStepExtension(it.first, it.second)}.toSet()
-        return x
+    private fun List<Point>.determineAntinodeLocationsOneStep(): Set<Point> {
+        return this
+            .toCombinedItemsList()
+            .flatMap{ oneStepExtension(it.first, it.second) }
+            .toSet()
     }
 
-    private fun List<Point>.determineAntinodeLocations2(): Set<Point> {
-        val x =  this.toCombinedItemsList().flatMap{lineExtension(it.first, it.second) }.toSet()
-        return x
+    private fun List<Point>.determineAntinodeLocationsInLine(): Set<Point> {
+        return this
+            .toCombinedItemsList()
+            .flatMap{ lineExtension(it.first, it.second) }
+            .toSet()
     }
-
 
     private fun oneStepExtension(p1: Point, p2: Point): Set<Point> {
         val deltaX = p1.x - p2.x
         val deltaY = p1.y - p2.y
+
         val newPoint1 = p1.plusXY(deltaX, deltaY)
         val newPoint2 = p2.plusXY(-deltaX, -deltaY)
         return setOf(newPoint1, newPoint2).intersect(gridFields)
@@ -48,20 +52,13 @@ class Day08(test: Boolean) : PuzzleSolverAbstract(test, puzzleName="Resonant Col
         val deltaX = p2.x - p1.x
         val deltaY = p2.y - p1.y
 
-        val extraPoints = mutableSetOf<Point>()
-        var currentPoint = p1
-        while (currentPoint in gridFields) {
-            extraPoints += currentPoint
-            currentPoint = currentPoint.plusXY(deltaX, deltaY)
-
-        }
-        currentPoint = p2
-        while (currentPoint in gridFields) {
-            extraPoints += currentPoint
-            currentPoint = currentPoint.plusXY(-deltaX, -deltaY)
-        }
-
-        return extraPoints
+        val oneDirection = generateSequence<Point>(p1) { aPoint -> aPoint.plusXY(deltaX, deltaY) }
+            .takeWhile { it in gridFields }
+            .toSet()
+        val otherDirection = generateSequence<Point>(p2) { aPoint -> aPoint.plusXY(-deltaX, -deltaY) }
+            .takeWhile { it in gridFields }
+            .toSet()
+        return oneDirection + otherDirection
     }
 
 }

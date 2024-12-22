36e6c079a9c2f806e4cdaed0cc8b880fd8a5a19f
Martin Schijf <martinschijf@bol.com>
2024-12-22 09:25:14 +0100

    December 22 - bit refactored, not much faster

--- src/main/kotlin/adventofcode/Day22.kt
+++ src/main/kotlin/adventofcode/Day22.kt
@@ -4,45 +4,39 @@ fun main() {
     Day22(test=false).showResult()
 }
 
-class Day22(test: Boolean) : PuzzleSolverAbstract(test, puzzleName="TBD", hasInputFile = true) {
+class Day22(test: Boolean) : PuzzleSolverAbstract(test, puzzleName="Monkey Market", hasInputFile = true) {
 
     override fun resultPartOne(): Any {
-        return inputLines.sumOf { it.toLong().next2000Secret() }
+        return inputLines.sumOf { it.toLong().nextSecret(2000) }
     }
 
     override fun resultPartTwo(): Any {
         val total = mutableListOf<Pair<String, Int>>()
         inputLines("example2").forEach {
-            val digitList = it.toLong().generateSecretSequence(2000)
-            val changesList = digitList.generateChanges()
-            val changeSequences = changesList.generateChangeSequences()
-            val sequenceToFirstPrice = changeSequences.sequenceToPrice()
-            total.addAll(sequenceToFirstPrice)
+            val digitPriceList = it.toLong().generateSecretPriceList(2000)
+            val changesList = digitPriceList.createChangeInPricesList()
+            val pricePerChangeSequenceList = changesList.createPricePerChangeSequenceList()
+            total.addAll(pricePerChangeSequenceList)
         }
-        val result = total.groupBy { it.first }.mapValues { it -> it.value.sumOf {pair -> pair.second} }
-        return result.values.max()
+        val result = total.groupBy ({ it.first }, { it.second }).mapValues { it.value.sum() }
+        return result.maxBy { it.value }.value
     }
 
-    private fun Long.nextSecret(): Long {
-        val secret = this
-        val firstRuleResult = ( (secret * 64L) xor secret ) % 16777216
-        val secondRuleResult = ( (firstRuleResult/32L) xor firstRuleResult ) % 16777216
-        val thirdRuleResult = ( (secondRuleResult*2048L) xor secondRuleResult ) % 16777216
-        return thirdRuleResult
-    }
-
-    private fun Long.next2000Secret(): Long {
+    private fun Long.nextSecret(n: Int = 1): Long {
         var secret = this
-        repeat(2000) {
-            secret = secret.nextSecret()
+        repeat(n) {
+            val firstRuleResult = ((secret * 64L) xor secret) % 16777216
+            val secondRuleResult = ((firstRuleResult / 32L) xor firstRuleResult) % 16777216
+            val thirdRuleResult = ((secondRuleResult * 2048L) xor secondRuleResult) % 16777216
+            secret = thirdRuleResult
         }
         return secret
     }
 
-    private fun Long.generateSecretSequence(n: Int): List<Int> {
+    private fun Long.generateSecretPriceList(sequenceLength: Int): List<Int> {
         var secret = this
-        val result = mutableListOf<Int>(secret.toInt() % 10)
-        repeat(n) {
+        val result = mutableListOf(secret.toInt() % 10)
+        repeat(sequenceLength) {
             secret = secret.nextSecret()
             val digit = (secret % 10)
             result.add(digit.toInt())
@@ -50,25 +44,21 @@ class Day22(test: Boolean) : PuzzleSolverAbstract(test, puzzleName="TBD", hasInp
         return result
     }
 
-    private fun List<Int>.generateChanges(): List<Pair<Int, Int>> {
-        val result = mutableListOf<Pair<Int, Int>>()
-        for (i in 1..<this.size) {
-            result.add(Pair (this[i], this[i]-this[i-1]))
-        }
-        return result
+    private fun List<Int>.createChangeInPricesList(): List<Pair<Int, Int>> {
+        return this.zipWithNext {a, b -> Pair(b, b-a) }
     }
 
-    private fun List<Pair<Int, Int>>.generateChangeSequences(): List<Pair<String, Int>> {
+    private fun List<Pair<Int, Int>>.createPricePerChangeSequenceList(): List<Pair<String, Int>> {
         return this
             .windowed(4, 1)
             .map { window ->
                 Pair(window.joinToString("") { it.second.toString() }, window.last().first)
             }
+            .groupBy { it.first }
+            .mapValues { it.value.first().second }
+            .toList()
     }
 
-    private fun List<Pair<String, Int>>.sequenceToPrice(): List<Pair<String, Int>> {
-        return this.groupBy { it.first }.mapValues { it.value.first().second }.toList()
-    }
 }
 
 

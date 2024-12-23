aed15b4edbe9ab70e97fcc7f630055afc8246fc9
Martin Schijf <martinschijf@bol.com>
2024-12-23 12:26:44 +0100

    December 23 - refactored and performanc improvements

--- src/main/kotlin/adventofcode/Day23.kt
+++ src/main/kotlin/adventofcode/Day23.kt
@@ -1,5 +1,6 @@
 package adventofcode
 
+import tool.mylambdas.collectioncombination.filterCombinedItems
 import kotlin.math.min
 
 fun main() {
@@ -9,103 +10,62 @@ fun main() {
 class Day23(test: Boolean) : PuzzleSolverAbstract(test, puzzleName="LAN Party", hasInputFile = true) {
 
     private val allConnections = inputLines
-        .map { Pair (it.split("-")[0], it.split("-")[1]) }
+        .map { Pair(it.split("-")[0], it.split("-")[1]) }
 
     private val allComputers = allConnections
         .flatMap { listOf(it.first, it.second) }.distinct()
         .associateWith { Computer(it) }
 
-    override fun resultPartOne(): Any {
+    init {
         allConnections.forEach { connection ->
             allComputers[connection.first]!!.addConnectedComputer(allComputers[connection.second]!!)
             allComputers[connection.second]!!.addConnectedComputer(allComputers[connection.first]!!)
         }
-        println("11440 is not correct")
-        println("4004 is too high")
-        println("1189 is not correct")
-        println("1170 is correct!")
-        return checkPart1()
     }
+
+
+    override fun resultPartOne(): Any {
+//        println("             : 1170")
+        return allComputers.flatMap { it.value.strongConnectionGroupsOfThree() }.distinct()
+            .filter { it.any { elt -> elt.name.startsWith("t") } }.size
+    }
+
 
     override fun resultPartTwo(): Any {
-        allConnections.forEach { connection ->
-            allComputers[connection.first]!!.addConnectedComputer(allComputers[connection.second]!!)
-            allComputers[connection.second]!!.addConnectedComputer(allComputers[connection.first]!!)
-        }
-//        println("vc")
-//        checkPart2(allComputers["vc"]!!)
-//        println()
-//        println("ka")
-//        checkPart2(allComputers["ka"]!!)
-
-//        checkPart2(allComputers["ab"]!!)
-        val result = allComputers.values.map { Pair(it.name, checkPart2(it)) }
+//        println("             : bo,dd,eq,ik,lo,lu,ph,ro,rr,rw,uo,wx,yg")
+        val result = allComputers.values.map { Pair(it.name, it.biggestStrongConnectedGroup()) }
         val max = result.maxOf { it.second }
-        return result.filter { it.second == max }.sortedBy { it.first }.joinToString (","){ it.first }
+        return result.filter { it.second == max }.sortedBy { it.first }.joinToString(",") { it.first }
     }
 
-    private fun checkPart1(): Int {
-        var count = 0
-        val computerList = if (test) allComputers.values.toList().sortedBy { it.name } else allComputers.values.toList()
-        for (i in 0..< computerList.size - 2) {
-            val setI = computerList[i].connections.toSet() + computerList[i]
-            for (j in i+1..< computerList.size - 1) {
-                val setJ = computerList[j].connections.toSet() + computerList[j]
-                val combinedSet2 = setI intersect setJ
-                if (combinedSet2.size >= 3) {
-                    for (k in j + 1..<computerList.size) {
-                        val setK = computerList[k].connections.toSet() + computerList[k]
-                        val combinedSet3 = combinedSet2 intersect setK
-                        if (combinedSet3.size >= 3) {
-                            if (computerList[i] in combinedSet3 && computerList[j] in combinedSet3 && computerList[k] in combinedSet3) {
-                                if (computerList[i].name.startsWith("t") || computerList[j].name.startsWith("t") || computerList[k].name.startsWith("t")) {
-                                    count++
-                                    if (test)
-                                        println("${computerList[i]} ${computerList[j]} ${computerList[k]}")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return count
-    }
-
-    fun checkPart2(computer: Computer): Int {
-        val xx = computer.connections.map { cn1 -> (cn1.connections.toSet() + cn1)  }
-        val yy = computer.connections.map { cn1 -> xx.count{ cn1 in it } }
-        val zz = yy.groupingBy { it }.eachCount()
-        if (false) {
-            println(computer.connections)
-            println(xx)
-            println(yy)
-            println(zz)
-        }
-        val bv = zz.maxBy { it.value }
-        return min(bv.key, bv.value) + 1
-    }
 }
 
 data class Computer(val name: String) {
-    val connections = mutableListOf<Computer>()
+    val connections = mutableSetOf<Computer>()
 
     fun addConnectedComputer(otherComputer: Computer) {
         connections.add(otherComputer)
     }
 
     override fun toString() = name
+
+    fun strongConnectionGroupsOfThree() : List<Set<Computer>> {
+        return connections.toList()
+            .filterCombinedItems { cn1, cn2 -> cn1 in cn2.connections }
+            .map{ setOf(this, it.first, it.second)}
+    }
+
+    fun biggestStrongConnectedGroup(): Int {
+//        val connectionGroupPerConnection = connections.map { cn -> Pair(cn, (cn.connections.toSet() + cn)) }
+//        val yy = connections.map { cn -> connectionGroupPerConnection.filter { cn in it.second }.map { it.first }.toSet() }
+//        val zz = yy.groupingBy { it }.eachCount()
+//        val bv = zz.maxBy { it.value }
+//        return min(bv.key.size, bv.value) + 1
+        val connectionGroupPerConnection = connections.map { cn -> (cn.connections.toSet() + cn)  }
+        val yy = connections.map { cn -> connectionGroupPerConnection.count{ cn in it } }
+        val zz = yy.groupingBy { it }.eachCount()
+        val bv = zz.maxBy { it.value }
+        return min(bv.key, bv.value) + 1
+    }
+
 }
-
-
-
-//fun printAllCombis(list: List<Char> = listOf('a', 'b', 'c', 'd', 'e')) {
-//    if (list.isEmpty()) {
-//        print("")
-//    } else {
-//        print(list[0])
-//        printAllCombis(list.drop(1))
-//        println()
-//    }
-//}
-//

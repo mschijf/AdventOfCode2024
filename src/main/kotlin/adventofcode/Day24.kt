9931511a29a0cd6409358008f98cb39f06630947
Martin Schijf <martinschijf@bol.com>
2024-12-25 06:47:20 +0100

    december 24: another commit, tried brute force and removed that again --> didn't work, even after many hours

--- src/main/kotlin/adventofcode/Day24.kt
+++ src/main/kotlin/adventofcode/Day24.kt
@@ -9,70 +9,45 @@ fun main() {
 class Day24(test: Boolean) : PuzzleSolverAbstract(test, puzzleName="Crossed Wires", hasInputFile = true) {
 
     private val splittedInput = inputLines.splitByCondition { it.isBlank() }
-    private val bitMap = splittedInput[0].map { it.substringBefore(": ") to it.substringAfter(": ").toInt()}.toMap().toMutableMap()
+    private val bitMap = splittedInput[0].map { it.substringBefore(": ") to it.substringAfter(": ").toInt()}.toMap()
     private val operationList = splittedInput[1].map {Operator.of(it)}
 
     override fun resultPartOne(): Any {
-        while ( operationList.any { !it.hasBeenCalculated(bitMap) } ) {
-            operationList.first { it.canBeCalculated(bitMap) }.execute(bitMap)
-        }
-        val resultZ = bitMap.filterKeys { it.startsWith("z") }.toSortedMap().values.joinToString("").reversed()
-        return resultZ.toLong(radix=2)
+        return calculateAll(bitMap.toMutableMap())
     }
 
     //checked: z** only right side of ->, x** and y** only on left side of ->
     override fun resultPartTwo(): Any {
-        while ( operationList.any { !it.hasBeenCalculated(bitMap) } ) {
-            operationList.first { it.canBeCalculated(bitMap) }.execute(bitMap)
+        return "No solution yet"
+    }
+
+    private fun makeOutputList(indexList: List<Int>): String {
+        return indexList.map {idx -> operationList[idx].resultName}.sorted().joinToString(",")
+    }
+
+    private fun swapOperationOutput(index1: Int, index2: Int) {
+//        operationList[index1].resultName = operationList[index2].resultName
+//            .also { operationList[index2].resultName = operationList[index1].resultName }
+        val tmp = operationList[index1].resultName
+        operationList[index1].resultName = operationList[index2].resultName
+        operationList[index2].resultName = tmp
+    }
+
+    private fun calculateAll(bitMap: Map<String, Int>): Long {
+        val mutBitMap = bitMap.toMutableMap()
+        val todo = operationList.toMutableSet()
+        while ( todo.isNotEmpty() ) {
+            val operation = operationList.firstOrNull { it.canBeCalculated(mutBitMap) } ?: return -1
+            operation.execute(mutBitMap)
+            todo -= operation
         }
-        val resultX = bitMap.filterKeys { it.startsWith("x") }.toSortedMap().values.joinToString("").reversed()
-        val resultY = bitMap.filterKeys { it.startsWith("y") }.toSortedMap().values.joinToString("").reversed()
-        val resultZ = bitMap.filterKeys { it.startsWith("z") }.toSortedMap().values.joinToString("").reversed()
-
-        val longX = resultX.toLong(radix=2)
-        val longY = resultY.toLong(radix=2)
-        val longZ = resultZ.toLong(radix=2)
-        val shouldBeZ = longX + longY
-
-        println("found    : $longX + $longY = $longZ")
-        println("should be: $longX + $longY = $shouldBeZ")
-
-        val shouldBeZBits = shouldBeZ.toString(radix = 2)
-        val diff = shouldBeZBits.mapIndexed { index, c -> if (resultZ[index] != c) '^' else ' ' }.joinToString("")
-
-        println()
-        println("found: $resultZ")
-        println("must : $shouldBeZBits")
-        println("       $diff")
-        println(diff.count { it == '^' })
-        println()
-        val firstDiff = diff.length - diff.lastIndexOf('^') - 1
-        val wrongResult = (if (firstDiff < 10) "z0" else "z") + firstDiff
-        println(wrongResult)
-        val allOps = findBackwardsTree(wrongResult)
-
-        println(allOps.size)
-        println(allOps.distinct().size)
-//
-//        val allall = (0..diff.length-1).flatMap { findBackwardsTree( (if (it < 10) "z0" else "z") + it) }
-//        println(allall.size)
-//        println(allall.distinct().size)
+        val resultZ = mutBitMap.filterKeys { it.startsWith("z") }.toSortedMap().values.joinToString("").reversed()
         return resultZ.toLong(radix=2)
     }
 
-    private fun findBackwardsTree(resultName: String, level: Int = 0):List<Operator> {
-        val operation = operationList.firstOrNull() { it.resultName == resultName }
-        if (operation != null) {
-            print("  ".repeat(level))
-            println(operation)
-            return findBackwardsTree(operation.op1, level+1) + findBackwardsTree(operation.op2, level+1) + operation
-        } else {
-            return emptyList()
-        }
-    }
 }
 
-data class Operator(val operatorType: String, val op1: String, val op2: String, val resultName: String) {
+data class Operator(val operatorType: String, val op1: String, val op2: String, var resultName: String) {
 
     fun hasBeenCalculated(bitMap: MutableMap<String, Int>) : Boolean {
         return bitMap.contains(resultName)
@@ -110,6 +85,70 @@ data class Operator(val operatorType: String, val op1: String, val op2: String, 
     }
 
 }
+
+
+//private fun findBackwardsTree(resultName: String, level: Int = 0):List<Operator> {
+//    val operation = operationList.firstOrNull() { it.resultName == resultName }
+//    if (operation != null) {
+////            print("  ".repeat(level))
+////            println(operation)
+//        return findBackwardsTree(operation.op1, level+1) + findBackwardsTree(operation.op2, level+1) + operation
+//    } else {
+//        return emptyList()
+//    }
+//}
+
+
+// calculateAll(bitMap.toMutableMap())
+//
+//val resultX = bitMap.filterKeys { it.startsWith("x") }.toSortedMap().values.joinToString("").reversed()
+//val resultY = bitMap.filterKeys { it.startsWith("y") }.toSortedMap().values.joinToString("").reversed()
+//val resultZ = bitMap.filterKeys { it.startsWith("z") }.toSortedMap().values.joinToString("").reversed()
+//
+//val longX = resultX.toLong(radix=2)
+//val longY = resultY.toLong(radix=2)
+//val longZ = resultZ.toLong(radix=2)
+//val shouldBeZ = longX + longY
+//
+//println("found    : $longX + $longY = $longZ")
+//println("should be: $longX + $longY = $shouldBeZ")
+//
+//val shouldBeZBits = shouldBeZ.toString(radix = 2)
+//val diff = shouldBeZBits.mapIndexed { index, c -> if (resultZ[index] != c) '^' else ' ' }.joinToString("")
+//
+//println()
+//println("found: $resultZ")
+//println("must : $shouldBeZBits")
+//println("       $diff")
+//println("wrong z values: ${diff.count { it == '^' }}")
+//println()
+//
+////        val firstDiff = diff.length - diff.lastIndexOf('^') - 1
+////        val wrongResult = (if (firstDiff < 10) "z0" else "z") + firstDiff
+////        println(wrongResult)
+////        val allOps = findBackwardsTree(wrongResult)
+//
+//diff.forEachIndexed() { index, ch ->
+//    if (ch == '^') {
+//        val revIndex = diff.length - index - 1
+//        val zzz = (if (revIndex < 10) "z0" else "z") + revIndex
+//        val operation = operationList.firstOrNull() { it.resultName == zzz }
+//        print("$zzz: ")
+//        print (operation)
+//        print("   ${resultZ[index]} -> ${shouldBeZBits[index]}")
+//        print("  (${findBackwardsTree(operation!!.resultName).distinct().size})")
+//        println()
+//    }
+//}
+//
+////        println(allOps.size)
+////        println(allOps.distinct().size)
+//
+////        val allall = (0..diff.length-1).filter{diff[it] == '^'}.flatMap { findBackwardsTree( (if (it < 10) "z0" else "z") + it) }
+////        println(allall.size)
+////        println(allall.distinct().size)
+//
+//return resultZ.toLong(radix=2)
 
 
 
